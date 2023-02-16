cea0cfd808593cfe674574d180d3cb13978fdf0d
refuogee <simonthevorwerk@gmail.com>
2023-02-16 12:45:02 +0200

    Initial Commit: Building expense tracker app. This is the initial commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+    <p class="text-3xl text-gray-700 font-bold mb-5">
+       Welcome!
+    </p>
+    <p class="text-gray-500 text-lg">
+       SvelteKit and Tailwind CSS in action
+    </p>
+</div>
