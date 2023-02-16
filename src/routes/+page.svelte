f32e4ee97f44ca90c8adcaba0ceaf3663b7ca8e4
refuogee <simonthevorwerk@gmail.com>
2023-02-16 14:18:31 +0200

    refactor: Added in flex-col for small devices which changes to flex-row on larger devices.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -51,7 +51,7 @@
             <p class="text-base p-1 mb-6 font-light">To add an expense input the required info and press add.</p>
             <form method="POST" action="?/new">
                 
-                <div class="flex -mx-3 mb-2">
+                <div class="flex flex-col md:flex-row -mx-3 mb-2">
                     
                     <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-city">
@@ -90,7 +90,7 @@
             {#each expenses as expense}
     
                 <form method="POST" action="?/remove">        
-                    <div class="flex -mx-3 mb-5 border-2 p-5 rounded bg-gray-200">
+                    <div class="flex flex-col md:flex-row -mx-3 mb-5 border-2 p-5 rounded bg-gray-200">
                         <input type="hidden" name="id" value={expense._id}>
             
                         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
@@ -121,7 +121,7 @@
                         </label>          
                         <input class="appearance-none block w-full disabled:bg-slate-100 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"  type="text" placeholder="{ (expense.amount).toLocaleString('en-US', { style: 'currency', currency: 'ZAR' }) }" name="amount" step="0.01" disabled>            
                         </div>
-                        <button class="bg-red-500 h-10 hover:bg-red-700 text-white font-bold mt-7 py-2 px-4 rounded"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                        <button class="bg-red-500 h-10 hover:bg-red-700 text-white font-bold mt-7 py-2 px-4 rounded mx-auto"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg></button>        
                     </div>
